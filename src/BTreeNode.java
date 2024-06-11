d924cc0917de63f18cf071d8a4327db2bdfe6c68
Pham Ngoc Phu <binpham@nexondv.com>
2024-06-11 18:12:37 +0700

    update split child

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -22,32 +22,42 @@ public class BTreeNode {
         // sao chép những keys cuối từ treeNode đến newNode
         // những key còn lại sẽ ở lại với tree node cux
         // Sao chép các con trỏ con: Vòng lặp for chạy từ 0 đến t-1
-        for(int keyIndex = 0; keyIndex < numberOfMaximumChildren; keyIndex++){
-            // sao chép các keys from t -> 2t - 1;
-            int index = keyIndex + minimumDegree;
-            newTreeNode.keys[keyIndex] = treeNode.keys[index];
-        }
-
+        copyKeys(treeNode, newTreeNode, minimumDegree);
 
         // nếu tree node không phải là leafnode sẽ cần sao chép các con trỏ con từ
         // treeNode  đầy sang treeNode mới
         if(!treeNode.leaf){
             // Sao chép các con trỏ con: Vòng lặp for chạy từ 0 đến t-1
-            for(int keyIndex = 0; keyIndex < minimumDegree; keyIndex++){
-                // sao chép các  node from t -> 2t - 1;
-                int index =  keyIndex + minimumDegree;
-                newTreeNode.children[keyIndex] = treeNode.children[index];
-            }
+            copyChildren(treeNode, newTreeNode, minimumDegree);
         }
 
         // reset number of keys at treenode old
         treeNode.currentNumberOfKeys = numberOfMaximumChildren;
 
-        // currentNumberOfKeys số number keys của parentNode
-        // keyIndex at treeNode
-        for(int keyIndex = currentNumberOfKeys; keyIndex >= childIndex + 1; keyIndex--){
+        //
+        insertChild(childIndex, newTreeNode);
+    }
 
+    private void copyChildren(BTreeNode sourceNode, BTreeNode targetNode, int minimumDegree) {
+        for (int childIndex = 0; childIndex < minimumDegree; childIndex++) {
+            int index = childIndex + minimumDegree;
+            targetNode.children[childIndex] = sourceNode.children[index];
         }
     }
+
+    private void copyKeys(BTreeNode sourceNode, BTreeNode targetNode, int minimumDegree) {
+        for (int keyIndex = 0; keyIndex < CommonUtil.getNumberOfMinimumKeyInANode(minimumDegree); keyIndex++) {
+            int index = keyIndex + minimumDegree;
+            targetNode.keys[keyIndex] = sourceNode.keys[index];
+        }
+    }
+
+    private void insertChild(int childIndex, BTreeNode newTreeNode) {
+        for (int j = currentNumberOfKeys; j >= childIndex + 1; j--) {
+            children[j + 1] = children[j];
+        }
+        children[childIndex + 1] = newTreeNode;
+    }
+
 
 }
